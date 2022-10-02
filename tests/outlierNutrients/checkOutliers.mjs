3d217f2ef39ea3f0cca9319dbcde4f42be844b0d
Augusto Lopes <augustolfp@gmail.com>
2022-10-02 14:44:44 -0300

    Add script to filter outlier macronutrients

---
+++ tests/outlierNutrients/checkOutliers.mjs
@@ -0,0 +1,58 @@
+import foodList from "../../originalData/foodList.json" assert { type: "json" };
+import fs from "fs";
+
+const simplifiedFoodList = foodList.map((food) => {
+  return {
+    description: food.description,
+    baseQty: food.base_qty,
+    baseUnit: food.base_unit,
+    proteins: food.attributes.protein?.qty,
+    proteinUnit: food.attributes.protein?.unit,
+    carbs: food.attributes.carbohydrate?.qty,
+    carbUnit: food.attributes.carbohydrate?.unit,
+    fats: food.attributes.lipid?.qty,
+    fatUnit: food.attributes.lipid?.unit,
+  };
+});
+
+let outlierProteins = simplifiedFoodList.filter((food) => {
+  if (typeof food.proteins === "string") return true;
+});
+
+let outlierCarbs = simplifiedFoodList.filter((food) => {
+  if (typeof food.carbs === "string") return true;
+});
+
+let outlierFats = simplifiedFoodList.filter((food) => {
+  if (typeof food.fats === "string") return true;
+});
+
+fs.writeFile(
+  "./results/outlierProteins.json",
+  JSON.stringify(outlierProteins),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+fs.writeFile(
+  "./results/outlierCarbs.json",
+  JSON.stringify(outlierCarbs),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+fs.writeFile(
+  "./results/outlierFats.json",
+  JSON.stringify(outlierFats),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
