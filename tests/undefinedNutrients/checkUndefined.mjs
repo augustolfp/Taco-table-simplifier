793d75562455f9eddb0e879114b118ef58c8b0e8
Augusto Lopes <augustolfp@gmail.com>
2022-10-02 14:33:32 -0300

    Add script to filter undefined macronutrients

---
+++ tests/undefinedNutrients/checkUndefined.mjs
@@ -0,0 +1,58 @@
+import foodList from "../../originalData/foodList.json" assert { type: "json" };
+import fs from "fs";
+
+const simplifiedFoodList = foodList.map((food) => {
+  return {
+    description: food.description,
+    baseQty: food.base_qty,
+    baseUnit: food.base_unit,
+    proteins: food.attributes.protein?.qty,
+    proteinUnit: food.attributes.protein?.unit,
+    carbs: food.attributes.carbohydrate?.qty,
+    carbUnit: food.attributes.carbohydrate?.unit,
+    fats: food.attributes.lipid?.qty,
+    fatUnit: food.attributes.lipid?.unit,
+  };
+});
+
+let undefinedProteins = simplifiedFoodList.filter((food) => {
+  if (!food.proteins) return true;
+});
+
+let undefinedCarbs = simplifiedFoodList.filter((food) => {
+  if (!food.carbs) return true;
+});
+
+let undefinedFats = simplifiedFoodList.filter((food) => {
+  if (!food.fats) return true;
+});
+
+fs.writeFile(
+  "./results/undefinedProteins.json",
+  JSON.stringify(undefinedProteins),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+fs.writeFile(
+  "./results/undefinedCarbs.json",
+  JSON.stringify(undefinedCarbs),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+fs.writeFile(
+  "./results/undefinedFats.json",
+  JSON.stringify(undefinedFats),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
