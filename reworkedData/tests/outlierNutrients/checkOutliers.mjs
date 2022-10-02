9a20927eb58c20e3458f3a2651b8227163b5dae9
Augusto Lopes <augustolfp@gmail.com>
2022-10-02 15:39:52 -0300

    Add script to test reworked table

---
+++ reworkedData/tests/outlierNutrients/checkOutliers.mjs
@@ -0,0 +1,14 @@
+import reworkedFoodList from "../../reworkedFoodList.json" assert { type: "json" };
+import fs from "fs";
+
+const outliers = reworkedFoodList.filter((food) => {
+  if (typeof food.proteins !== "number") return true;
+  if (typeof food.carbs !== "number") return true;
+  if (typeof food.fats !== "number") return true;
+});
+
+fs.writeFile("./results/outliers.json", JSON.stringify(outliers), (err) => {
+  if (err) {
+    throw err;
+  }
+});
