b8252ed5142caf633f5036f975c7925e84907916
Augusto Lopes <augustolfp@gmail.com>
2022-10-02 14:55:25 -0300

    Add script to ensure that all macronutrients are in grams

---
+++ tests/unitsTest/outlierUnits.mjs
@@ -0,0 +1,58 @@
+import foodList from "../../originalData/foodList.json" assert { type: "json" };
+import fs from "fs";
+
+const simplifiedFoodList = foodList.map((food) => {
+  return {
+    description: food.description,
+    baseQty: food.base_qty,
+    baseUnit: food.base_unit,
+    proteins: food.attributes.protein?.qty,
+    proteinUnit: food.attributes.protein?.unit,
+    carbs: food.attributes.carbohydrate?.qty,
+    carbUnit: food.attributes.carbohydrate?.unit,
+    fats: food.attributes.lipid?.qty,
+    fatUnit: food.attributes.lipid?.unit,
+  };
+});
+
+let outlierProteinUnits = simplifiedFoodList.filter((food) => {
+  if (food.proteins && food.proteinUnit !== "g") return true;
+});
+
+let outlierCarbUnits = simplifiedFoodList.filter((food) => {
+  if (food.carbs && food.carbUnit !== "g") return true;
+});
+
+let outlierFatUnits = simplifiedFoodList.filter((food) => {
+  if (food.fats && food.fatUnit !== "g") return true;
+});
+
+fs.writeFile(
+  "./results/outlierProteinUnits.json",
+  JSON.stringify(outlierProteinUnits),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+fs.writeFile(
+  "./results/outlierCarbUnits.json",
+  JSON.stringify(outlierCarbUnits),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+fs.writeFile(
+  "./results/outlierFatUnits.json",
+  JSON.stringify(outlierFatUnits),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
